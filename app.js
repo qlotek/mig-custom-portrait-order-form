9d2bc42d28eb0518de96ecf54605e1c548e4ed06
Chris Papastefanou <papastefanou@gmail.com>
2021-04-21 08:58:49 +0200

    Moving forward

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
+// DOM Objects
+
+//const size = document.querySelector('#size');
+const people = document.querySelector('#people');
+const technique = document.querySelector('#technique');
+const pricedisplay = document.querySelector('#pricedisplay');
 const sampleimage = document.querySelector('#sampleimage');
 
-const technique = document.querySelector('#technique');
+let price;
 
-technique.addEventListener('input', updateSampleImage());
+// Prices in SEK
+const basePrice = 1100;
+const perPerson = 1200;
+const pencil = 750;
+const watercolorBw = 750;
+const watercolorFullcolor = 750;
+
+// Event listeners
+/* document
+  .querySelector('#technique')
+  .addEventListener('change', updateSampleImage(), false); */
+
+document.querySelector('#size').addEventListener('input', updatePrice());
+
+people.addEventListener('click', hjalmar());
 
 function updateSampleImage() {
   console.log(technique.value);
   sampleimage.src = 'pix/sampleimage-' + technique.value + '.jpg';
+}
+
+function updatePrice() {
+  price = basePrice + perPerson * people.value;
+  pricedisplay.innerHTML = price + ' SEK.';
+  console.log('Estimated price ' + price + ' SEK.');
+}
+
+function hjalmar() {
+  console.log('Hjall!');
 }
